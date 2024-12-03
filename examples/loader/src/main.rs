bc12f4a64505e096818116f0d9ed8b4cceea1beb
Ansen <alj79@sohu.com>
2024-12-03 21:03:14 +0800

    step 2

--- examples/loader/src/main.rs
+++ examples/loader/src/main.rs
@@ -8,13 +8,22 @@ const PLASH_START: usize = 0xffff_ffc0_2200_0000;
 
 #[cfg_attr(feature = "axstd", no_mangle)]
 fn main(){
-    let apps_start = PLASH_START as *const u8;
-    let apps_size = 32;
+    let load_start = PLASH_START as *const u8;
+    let load_size = 32;
 
     println!("Load payload ..");
 
-    let code = unsafe {core::slice::from_raw_parts(apps_start, apps_size)};
-    println!("content: {:?}: ", code);
+    let load_code = unsafe {core::slice::from_raw_parts(load_start, load_size)};
+    println!("load code {:?}; address [{:?}]", load_code, load_code.as_ptr());
+    
+    const RUN_START: usize = 0xffff_ffc0_8010_0000;
+
+    let run_code = unsafe {
+        core::slice::from_raw_parts_mut(RUN_START as *mut u8, load_size)
+    };
+    run_code.copy_from_slice(load_code);
+    println!("run code {:?}; address [{:?}]", run_code, run_code.as_ptr());
+
     println!("Load payload ok!");
 }
 
