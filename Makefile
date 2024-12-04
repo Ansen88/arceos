84fa37c586c7d6ca36a29971de11e6d0275d1ebd
Ansen <alj79@sohu.com>
2024-12-04 15:52:19 +0800

    step 2

--- Makefile
+++ Makefile
@@ -50,7 +50,8 @@ GRAPHIC ?= n
 BUS ?= pci
 
 DISK_IMG ?= disk.img
-QEMU_LOG ?= n
+#QEMU_LOG ?= n
+QEMU_LOG ?= y
 NET_DUMP ?= n
 NET_DEV ?= user
 VFIO_PCI ?=
--- examples/loader/src/main.rs
+++ examples/loader/src/main.rs
@@ -1,21 +1,25 @@
 #![cfg_attr(feature = "axstd", no_std)]
 #![cfg_attr(feature = "axstd", no_main)]
+#![feature(asm_const)]
 
-#![cfg(feature = "axstd")]
+#[cfg(feature = "axstd")]
 use axstd::println;
 
 const PLASH_START: usize = 0xffff_ffc0_2200_0000;
 
 #[cfg_attr(feature = "axstd", no_mangle)]
-fn main(){
+fn main() {
     let load_start = PLASH_START as *const u8;
-    let load_size = 32;
+    let load_size = 32; // Dangerous!!! We need to get accurate size of apps.
 
-    println!("Load payload ..");
+    println!("Load payload ...");
 
-    let load_code = unsafe {core::slice::from_raw_parts(load_start, load_size)};
+    let load_code = unsafe { core::slice::from_raw_parts(load_start, load_size) };
     println!("load code {:?}; address [{:?}]", load_code, load_code.as_ptr());
-    
+
+    // app running aspace
+    // SBI(0x80000000) -> App <- Kernel(0x80200000)
+    // va_pa_offset: 0xffff_ffc0_0000_0000
     const RUN_START: usize = 0xffff_ffc0_8010_0000;
 
     let run_code = unsafe {
@@ -25,5 +29,14 @@ fn main(){
     println!("run code {:?}; address [{:?}]", run_code, run_code.as_ptr());
 
     println!("Load payload ok!");
-}
 
+    println!("Execute app ...");
+
+    // execute app
+    unsafe { core::arch::asm!("
+        li      t2, {run_start}
+        jalr    t2
+        j       .",
+        run_start = const RUN_START,
+    )}
+}
