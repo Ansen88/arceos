ba59544395e6edc1e5dc31e7b6620e94c89d60d2
Ansen <alj79@sohu.com>
2024-12-05 21:24:30 +0800

    exercise 2

--- examples/loader/src/main.rs
+++ examples/loader/src/main.rs
@@ -7,13 +7,19 @@ use axstd::println;
 const PLASH_START: usize = 0xffff_ffc0_2200_0000;
 
 #[repr(C)]
+struct SubHeader{
+    entry: u64,     // virtual address
+    data_len: u64,  // programer's length
+}
+
+#[repr(C)]
 struct Header{
     magic: [u8;16], // anlj
     class: [u8;8],  // anlj64
     endian: u64,    // little/big
     machine: u64,   // architecture
-    data_len: u64,   // programer's length
-    entry: u64,   // virtual address
+    app_num: u64,   // programer's length
+    subheader: [SubHeader; 2],
 }
 
 #[cfg_attr(feature = "axstd", no_mangle)]
@@ -24,17 +30,37 @@ fn main() {
     let header_ptr = unsafe {
         slice.as_ptr() as *const Header
     };
-
-    let apps_start = (PLASH_START + header_size) as *const u8;
     let header = unsafe {
         &*header_ptr
     };
-    let apps_size = header.data_len as usize; // Dangerous!!! We need to get accurate size of apps.
+
+    let app_num = header.app_num;
+    let app_start = PLASH_START + header_size;
+    println!("apps'num is {app_num}");
+    println!("header_size is {header_size}");
+
+    let app0_size = header.subheader[0].data_len as usize;
+    let app1_size = header.subheader[1].data_len as usize;
+
+    let app0_start_ptr = app_start as *const u8;
+    let app0_start = unsafe{
+        &*app0_start_ptr
+    };
+
+    let app1_start_ptr = (app_start + app0_size) as *const u8;
+    let app1_start = unsafe{
+        &*app1_start_ptr
+    };
 
     println!("Load payload ...");
 
-    let code = unsafe { core::slice::from_raw_parts(apps_start, apps_size) };
-    println!("content: {:?}: ", code);
+    let code = unsafe { core::slice::from_raw_parts(app0_start, app0_size) };
+    println!("app0 size is {}", app0_size);
+    println!("app0's content: {:?}: ", code);
+
+    let code = unsafe { core::slice::from_raw_parts(app1_start, app1_size) };
+    println!("app1 size is {}", app1_size);
+    println!("app1's content: {:?}: ", code);
 
     println!("Load payload ok!");
 }
